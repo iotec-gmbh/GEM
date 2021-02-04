f919a2cb87f4ba3772e33a9f06c902134e78d504
Simon Kerssen <simon.kerssen@iotec-gmbh.de>
2021-02-04 10:31:17 +0100

    Added ifndef checks to disable GLCD and/or U8G2 support (#2)
    * Added ifndef checks to disable GLCD and/or U8G2 support
    
    * Apply suggestions from code review
    
    Co-authored-by: TinaJ_kay <44362472+TylerBird@users.noreply.github.com>
    
    Co-authored-by: TinaJ_kay <44362472+TylerBird@users.noreply.github.com>

--- src/config.h
+++ src/config.h
@@ -1,3 +1,11 @@
-#include "config/enable-glcd.h"         // Enable AltSerialGraphicLCD version of GEM
-#include "config/enable-u8g2.h"         // Enable U8g2 version of GEM
+// Automatically enabled GLCD support. Can be disabled by adding the `GEM_DONT_USE_GLCD` define to your project level. 
+#ifndef GEM_DONT_USE_GLCD
+    #include "config/enable-glcd.h"         // Enable AltSerialGraphicLCD version of GEM
+#endif
+
+// Automatically enabled U8G2 support. Can be disabled by adding the `GEM_DONT_USE_U8G2` define to your project level. 
+#ifndef GEM_DONT_USE_U8G2
+    #include "config/enable-u8g2.h"         // Enable U8g2 version of GEM
+#endif
+
 #include "config/support-float-edit.h"  // Support for editable float and double variables (option selects support them regardless of this setting)
